60b5ee7acd76a3ab20f3fffc6e830c7bcf5ae824
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2023-12-12 19:09:18 +0530

    Landing Section Content with Chakra UI Components

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const greeting = "Hello, I am Dwaraka!";
+const bio1 = "A Frontend Developer";
+const bio2 = "Specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -15,7 +15,12 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack spacing={4}>
+      <Heading as='h1' >{greeting}</Heading>
+      <Avatar name='Dwarakanath Akkala' src='https://dwarakanath-akkala.netlify.app/assets/img/perfil.webp' size='2xl' />
+      <Heading as='h2'>{bio1}</Heading>
+      <Heading as='h3'>{bio2}</Heading>
+    </VStack>
   </FullScreenSection>
 );
 
